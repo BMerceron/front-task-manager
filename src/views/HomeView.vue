d55502355d20cb67eb4d782512377409c6289819
Bastien Merceron <bastien.merceron@conserto.pro>
2023-09-27 16:21:59 +0200

    add floating squares css html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <AppHeader :user="user" @disconnect="disconnect()" />
+  <FloatingSquares />
 </template>
 
 <script setup lang="ts">
 import AppHeader from '@/components/navigation/AppHeader.vue'
+import FloatingSquares from '@/components/animations/FloatingSquares.vue'
 import { useUserProfilStore } from '@/stores/userProfil'
 import { useRouter } from 'vue-router'
 
